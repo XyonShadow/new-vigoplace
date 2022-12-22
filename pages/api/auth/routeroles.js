f234ead798bd0b27cf84f9164f40aab137beacb5
chigozie <ezechukwuchigozie@gmail.com>
2022-12-22 10:37:38 +0100

    fetch routes roles from db

---
+++ pages/api/auth/routeroles.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+
+export default async function getRouteRoles(req, res) {
+//   const result = await axios.get("localhost:3000/api/admin/console/routeroles")
+  const result = await axios.get("https://vigoplace.com/server/api/admin/console/routeroless")
+        .then((res) => console.log(res.data, 'res o'))
+        .then((res) => res.data)
+        .catch((err) => console.log(err, 'err'))
+
+    // Get data from your database
+    // console.log(result.data, 'resultt')
+    res.status(200).json(result);
+    // res.status(200).json({users: [{name: 'John'}, {name: 'Jane'}]});
+  }
