0448e667384a74283c13d01d27819a1905b1d90f
chigozie <ezechukwuchigozie@gmail.com>
2023-01-18 16:23:56 +0100

    added card component to display balances

---
+++ src/components/dashboard/balanceCard.js
@@ -0,0 +1,51 @@
+import { Avatar, Box, Card, CardContent, Grid, Typography } from '@mui/material';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import MoneyIcon from '@mui/icons-material/Money';
+
+export const BalanceCard = ({balance, ...props}) => (
+  <Card
+    // sx={{ height: '100%' }}
+    {...props}
+  >
+    <CardContent>
+      <Grid
+        container
+        spacing={3}
+        sx={{ justifyContent: 'space-between' }}
+      >
+        <Grid item>
+          <Typography
+            color="textSecondary"
+            gutterBottom
+            variant="h4"
+          >
+            PayStack Balance
+          </Typography>
+          <Typography
+            // color="textPrimary"
+            variant="h3"
+            align='center'
+            color='green'
+          >
+            {balance.balance}
+          </Typography>
+        </Grid>
+      </Grid>
+      <Box
+        sx={{
+          pt: 2,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        <Typography
+          color="error"
+          variant="h6"
+          align='center'
+        >
+          available balance too low for naira wallet balances
+        </Typography>
+      </Box>
+    </CardContent>
+  </Card>
+);
---
+++ src/components/dashboard/userBalanceCard.js
@@ -0,0 +1,38 @@
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  Chip,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import MoneyIcon from "@mui/icons-material/Money";
+import { BorderColor } from "@mui/icons-material";
+
+export const UserBalanceCard = ({ usersBalance, ...props }) => (
+  <Card
+    // sx={{ height: '100%' }}
+    {...props}
+  >
+    <CardContent>
+      <Grid item>
+        <Typography color="textSecondary" gutterBottom variant="h4">
+          User Wallet Balances
+        </Typography>
+        {usersBalance
+          ? usersBalance.map((balance) => (
+              <>
+                <Typography color="green" variant="h2" align="center">
+                  {`${balance.SCSymbol}${balance.amount}`}
+                </Typography>
+                <Divider />
+              </>
+            ))
+          : null}
+      </Grid>
+    </CardContent>
+  </Card>
+);
