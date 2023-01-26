6acca6f65d96d5e321f9b1004e1d0ee658a62746
chigozie <ezechukwuchigozie@gmail.com>
2023-01-26 14:21:59 +0100

    added settings page with passwprd update and pin generation for admin

--- pages/settings.js
+++ pages/settings.js
@@ -1,8 +1,68 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import AdminSecurity from '../src/components/AdminSecurity';
+
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 
 export default function Settings() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <h1>Settings Coming soon...</h1>
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange}   textColor="inherit" centered scrollButtons="auto" aria-label="admin settings">
+          <Tab label="General" {...a11yProps(0)} />
+          <Tab label="Security" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+       Coming soon...
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <AdminSecurity/>
+      </TabPanel>
+    </Box>
   )
 }
 
---
+++ src/components/AdminSecurity.js
@@ -0,0 +1,265 @@
+import React from 'react'
+import { useState } from 'react';
+import { Box, Button, Card, CardContent, CardHeader, Divider, TextField, Container, Typography } from '@mui/material';
+import {useSession } from "next-auth/react";
+import {
+    useMutation,
+  } from "@tanstack/react-query";
+  import axios from "axios";
+import LoadingButton from '@mui/lab/LoadingButton';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import Slide from '@mui/material/Slide';
+
+
+
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
+function AdminSecurity() {
+    const getUser = useSession();
+    const user = getUser?.data?.user;
+
+    const [pinToast, setPinToast] = React.useState({
+        error: false,
+        success: false
+    });
+    const [passwordToast, setPasswordToast] = React.useState({
+        error: false,
+        success: false
+    });
+
+
+    const [password, setPassword] = useState({
+        oldPassword: '',
+        newPassword: ''
+      });
+
+    const [approval, setApproval] = useState({
+        oldPin: '',
+        pin: ''
+      });
+    
+      const handlePassword = (event) => {
+        setPassword({
+          ...password,
+          [event.target.name]: event.target.value
+        });
+      };
+      
+      const handlePinChange = (event) => {
+        setApproval({
+          ...approval,
+          [event.target.name]: event.target.value
+        });
+      };
+
+      const updatePin = async ({pin, oldPin}) => {
+        const setpin = await axios.post(
+        //   "http://localhost:3001/api/admin/console/approvalpin",
+          'https://vigoplace.com/server/api/admin/console/approvalpin',
+          { pin, oldPin },
+          {
+            headers: {
+              Authorization: user?.token,
+            },
+          }
+        );
+        return setpin;
+      };
+    
+      const updatePinMutation = useMutation({
+        mutationKey: ["updatePin"],
+        mutationFn: updatePin,
+        onError: async (error) => {
+            setPinToast({...pinToast, error: true});
+        },
+        onSuccess: () => {
+            setPinToast({...pinToast, success: true});
+          },
+      });
+
+      const changePassword = async ({oldPassword, newPassword}) => {
+        const password = await axios.post(
+        //   "http://localhost:3001/api/admin/console/password/update",
+          'https://vigoplace.com/server/api/admin/console/password/update',
+          { oldPassword, newPassword },
+          {
+            headers: {
+              Authorization: user?.token,
+            },
+          }
+        );
+        return password;
+      };
+    
+      const updatePasswordMutation = useMutation({
+        mutationKey: ["updatePassword"],
+        mutationFn: changePassword,
+        onError: async (error) => {
+            setPasswordToast({...passwordToast, error: true});
+        },
+        onSuccess: (msg) => {
+            setPasswordToast({...passwordToast, success:true});
+          },
+      });
+
+      const handleClose = (event, reason) => {
+        setPinToast({error: false, success: false});
+        setPasswordToast({error: false, success: false});
+      };
+
+  return (
+    <>
+       <Snackbar TransitionComponent={Slide} open={pinToast.error} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="warning" sx={{ width: '100%' }}>
+          {updatePinMutation.error?.response?.data?.message}
+        </Alert>
+      </Snackbar>
+       <Snackbar TransitionComponent={Slide} open={pinToast.success} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          {updatePinMutation?.data?.data?.message}
+        </Alert>
+      </Snackbar>
+
+       <Snackbar TransitionComponent={Slide} open={passwordToast.error} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="warning" sx={{ width: '100%' }}>
+          {updatePasswordMutation.error?.response?.data?.message}
+        </Alert>
+      </Snackbar>
+       <Snackbar TransitionComponent={Slide} open={passwordToast.success} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          {updatePasswordMutation?.data?.data?.message}
+        </Alert>
+      </Snackbar>
+    <Box
+    component="main"
+    sx={{
+      flexGrow: 1,
+    //   py: 8
+    }}
+  >
+    <Container maxWidth="lg">
+      <Box sx={{ pt: 3 }}>
+         <form>
+      <Card>
+        <CardHeader
+          subheader=""
+          title="Change Password"
+        />
+        <Divider />
+        <CardContent>
+          <TextField
+            fullWidth
+            label="Old Password"
+            margin="normal"
+            name="oldPassword"
+            onChange={handlePassword}
+            type="password"
+            value={password.oldPassword}
+            variant="outlined"
+          />
+          <TextField
+            fullWidth
+            label="New password"
+            margin="normal"
+            name="newPassword"
+            onChange={handlePassword}
+            type="password"
+            value={password.newPassword}
+            variant="outlined"
+          />
+        </CardContent>
+        <Divider />
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            p: 2
+          }}
+        >
+           <LoadingButton
+          variant="contained" 
+          color="primary"
+          loading={updatePasswordMutation.isLoading}
+          disabled={password.oldPassword === '' || password.newPassword === '' || password.oldPassword.length <=5 || password.newPassword.length <=5}
+          onClick={()=>{
+            updatePasswordMutation.mutate(password)
+            setPassword({oldPassword: '', newPassword: ''})
+          }}>Change Password</LoadingButton>
+        </Box>
+      </Card>
+    </form>
+      </Box>
+    </Container>
+
+    <Container maxWidth="lg">
+{/* <Typography
+  sx={{ mb: 3 }}
+  variant="h4"
+>
+  Settings
+</Typography> */}
+<Box sx={{ pt: 3 }}>
+   {/* <form> */}
+<Card>
+  <CardHeader
+    subheader="if this is your first approval pin kindly use 000000 as old pin"
+    title="Set Approval Pin"
+  />
+  <Divider />
+  <CardContent>
+    <TextField
+      fullWidth
+      label="Old Pin"
+      margin="normal"
+      name="oldPin"
+      onChange={handlePinChange}
+      type="number"
+      value={approval.oldPin}
+      variant="outlined"
+    />
+    <TextField
+      fullWidth
+      label="New Pin"
+      margin="normal"
+      name="pin"
+      onChange={handlePinChange}
+      type="number"
+      value={approval.pin}
+      variant="outlined"
+    />
+  </CardContent>
+  <Divider />
+  <Box
+    sx={{
+      display: 'flex',
+      justifyContent: 'flex-end',
+      p: 2
+    }}
+  >
+    <LoadingButton
+    variant="contained" 
+    color="primary"
+    loading={updatePinMutation.isLoading}
+    disabled={approval.pin === '' || approval.oldPin === '' || approval.pin.length <=5 || approval.oldPin.length <=5}
+    onClick={()=>{
+      updatePinMutation.mutate(approval)
+      setApproval({pin: '', oldPin: ''})
+    }}>Update</LoadingButton>
+  </Box>
+</Card>
+{/* </form> */}
+</Box>
+</Container>
+
+  
+  </Box>
+  </>
+
+  )
+}
+
+export default AdminSecurity
