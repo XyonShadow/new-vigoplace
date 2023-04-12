ed824e433cec5217d0d9a29dfa682b74db93d3d6
chigozie <ezechukwuchigozie@gmail.com>
2023-04-12 15:32:58 +0100

    added open to new tab

--- pages/users.js
+++ pages/users.js
@@ -779,7 +779,7 @@ const Users = () => {
                 // disabled={!table.getIsSomeRowsSelected('fullname')}
                 variant="contained"
               >
-             <Link href={`https://web.vigoplace.com/profile/${row.original.id}?adt=${user?.token}`}>vigoplace web profile</Link>
+             <Link target="_blank"  href={`https://web.vigoplace.com/profile/${row.original.id}?adt=${user?.token}`}>vigoplace web profile</Link>
              {/* <Link href={`http://localhost:3002/profile/${row.original.id}?adt=${user?.token}`}>Goto Merchant</Link> */}
               </Button>
             </MenuItem>,
