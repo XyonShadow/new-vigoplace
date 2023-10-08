d8c0dbde28734cd242233ff18460825eb5db161f
Emmanuel Essien <xxcel4ryll@gmail.com>
2023-10-09 00:26:25 +0100

    deploy updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,37 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: "AdminConsole",
+//       script: "npm run start",
+//       watch: false,
+//       ignore_watch: ["node_modules"],
+//       env_production: {
+//         NODE_ENV: "production"
+//       },
+//     },
+//   ],
+// };
+
+
+require('dotenv').config();
+
 module.exports = {
   apps: [
     {
-      name: "AdminConsole",
-      script: "npm run start",
-      watch: false,
+      name: `AdminConsole`,
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+		  wait_ready: true,
       ignore_watch: ["node_modules"],
-      env_production: {
-        NODE_ENV: "production"
+      port: 5530,
+      env_development: {
+        APP_ENV: 'Development',
+        PORT: 5530,
       },
+      env_production: {
+        APP_ENV: 'Production',
+        PORT: 5530,
+      }
     },
   ],
 };
