ce91e948e19150fc4438e792ffa502c65cc2614d
Emmanuel Essien <xxcel4ryll@gmail.com>
2023-10-09 01:03:45 +0100

    deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,37 +1,38 @@
-// module.exports = {
-//   apps: [
-//     {
-//       name: "AdminConsole",
-//       script: "npm run start",
-//       watch: false,
-//       ignore_watch: ["node_modules"],
-//       env_production: {
-//         NODE_ENV: "production"
-//       },
-//     },
-//   ],
-// };
-
-
-require('dotenv').config();
-
 module.exports = {
   apps: [
     {
-      name: `AdminConsole`,
-      script: 'node_modules/next/dist/bin/next',
-      args: 'start',
-		  wait_ready: true,
+      name: "AdminConsole",
+      script: "npm run dev",
+      watch: false,
+      port: 5000,
       ignore_watch: ["node_modules"],
-      port: 5530,
-      env_development: {
-        APP_ENV: 'Development',
-        PORT: 5530,
-      },
       env_production: {
-        APP_ENV: 'Production',
-        PORT: 5530,
-      }
+        NODE_ENV: "production"
+      },
     },
   ],
 };
+
+
+// require('dotenv').config();
+
+// module.exports = {
+//   apps: [
+//     {
+//       name: `AdminConsole`,
+//       script: 'node_modules/next/dist/bin/next',
+//       args: 'start',
+// 		  wait_ready: true,
+//       ignore_watch: ["node_modules"],
+//       port: 5530,
+//       env_development: {
+//         APP_ENV: 'Development',
+//         PORT: 5530,
+//       },
+//       env_production: {
+//         APP_ENV: 'Production',
+//         PORT: 5530,
+//       }
+//     },
+//   ],
+// };
