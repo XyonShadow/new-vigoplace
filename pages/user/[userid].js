7a60987169f23949583ac5f0495f738515c4f573
Dave <uchennadavid2404@gmail.com>
2023-12-28 00:29:56 +0100

    Finished the charges setting page

--- pages/user/[userid].js
+++ pages/user/[userid].js
@@ -600,7 +600,7 @@ const Users = () => {
   };
 
   const createTicketMutation = useMutation({
-    mutationKey: ["postYesDebitUser"],
+    mutationKey: ["createTicketUser"],
     mutationFn: createTicket,
     onSuccess: () => {
       queryClient.invalidateQueries("fetchSingleUser");
@@ -1235,7 +1235,7 @@ const Users = () => {
                           id="demo-dialog-select"
                           value={categoryId}
                           onChange={handleCategoryId}
-                          input={<OutlinedInput label="Category" />}
+                          input={<OutlinedInput label="Select an issue" />}
                           fullWidth
                           style={{ marginTop: "20px" }}
                         >
--- src/components/Charge.js
+++ src/components/Charge.js
@@ -36,33 +36,58 @@ function Charges() {
     error: false,
     success: false,
   });
+  const [currentCharge, setCurrentCharge] = useState("");
   const [openModal, setOpenModal] = React.useState(false);
   const [charges, setCharges] = useState({
-    currencyId: "", 
+    currencyId: "",
     operation: "",
-    newChargeValue: "", 
+    newChargeValue: "",
   });
 
-  const handleChargeChange = (event) => {
+  const handleChargeChange = async (event) => {
+    const { name, value } = event.target;
     setCharges({
       ...charges,
-      [event.target.name]: event.target.value,
+      [name]: value,
     });
+
+    if (name === "operation") {
+      try {
+        const response = await axios.get(
+          `https://vigoplace.com/server/api/admin/settings/charges/${charges.currencyId}/${value}`,
+          //`http://localhost:4000/api/admin/settings/charges/${charges.currencyId}/${value}`,
+          {
+            headers: {
+              Authorization: user?.token,
+            },
+          }
+        );
+        //console.log(response);
+        setCurrentCharge(response.data.charge);
+      } catch (error) {
+        setCurrentCharge("");
+      }
+    }
   };
 
   const operations = [
-    { id: "vote", label: "Vote" },
-    { id: "payout", label: "Payout" },
-    { id: "deposit", label: "Deposit" },
-    { id: "rent", label: "Rent" },
-    { id: "transfer", label: "Transfer" },
-    { id: "gift", label: "Gift" },
-    { id: "donation", label: "Donation" },
-    { id: "support", label: "Support" },
-    { id: "subscription", label: "Subscription" },
-    { id: "buy", label: "Buy" },
-    { id: "service", label: "Service" },
-    // Add other operations as needed
+    { id: "vote", label: "Contest Vote Charge" },
+    { id: "payout", label: "Payout Charge" },
+    { id: "deposit", label: "Deposit Charge" },
+    { id: "rent", label: "Rent Charge" },
+    { id: "transfer", label: "Transfer Charge" },
+    { id: "gift", label: "Gift Charge" },
+    { id: "donation", label: "Fundraising Charge" },
+    { id: "support", label: "Support Charge" },
+    { id: "subscription", label: "Subscription Charge" },
+    { id: "buy", label: "Buying Charge" },
+    { id: "service", label: "Service Purchase Charge" },
+    { id: "product", label: "Product Charge" },
+  ];
+
+  const currencies = [
+    { id: "175", label: "Naira" },
+    { id: "250", label: "USD" },
   ];
 
   const updateCharges = async ({ currencyId, operation, newChargeValue }) => {
@@ -141,41 +166,75 @@ function Charges() {
                     <Select
                       value={charges.currencyId}
                       onChange={handleChargeChange}
-                      inputProps={{
-                        name: "currencyId",
-                      }}
+                      input={
+                        <OutlinedInput
+                          label="Select a Currency"
+                          name="currencyId"
+                        />
+                      }
+                      // inputProps={{
+                      //   name: "currencyId",
+                      // }}
                       style={{ marginTop: "20px" }}
                     >
-                      <MenuItem value={175}>Naira</MenuItem>
-                      <MenuItem value={250}>Usd</MenuItem>
+                      {currencies.map((op) => (
+                        <MenuItem
+                          key={op.id}
+                          value={op.id}
+                          style={{ marginBottom: "5px" }}
+                        >
+                          {op.label}
+                        </MenuItem>
+                      ))}
+                      {/* <MenuItem value={175}>Naira</MenuItem>
+                      <MenuItem value={250}>USD</MenuItem> */}
                     </Select>
                   </FormControl>
 
                   <FormControl fullWidth>
-                    <InputLabel
-                      //id="demo-dialog-select-label"
-                      style={{ marginTop: "18px" }}
-                    >
+                    <InputLabel style={{ marginTop: "18px" }}>
                       Select an Operation
                     </InputLabel>
                     <Select
-                      //labelId="demo-dialog-select-label"
-                      //id="demo-dialog-select"
+                      // labelId="demo-dialog-select-label"
+                      // id="demo-dialog-select"
                       value={charges.operation}
                       onChange={handleChargeChange}
                       //   inputProps={{
                       //     name: "operation",
                       //   }}
-                      input={<OutlinedInput label="Charge" name="operation" />}
+                      input={
+                        <OutlinedInput
+                          label="Select an Operation"
+                          name="operation"
+                        />
+                      }
                       style={{ marginTop: "20px" }}
                     >
                       {operations.map((op) => (
-                        <MenuItem key={op.id} value={op.id}>
+                        <MenuItem
+                          key={op.id}
+                          value={op.id}
+                          style={{ marginBottom: "10px" }}
+                        >
                           {op.label}
                         </MenuItem>
                       ))}
                     </Select>
                   </FormControl>
+
+                  {/* Paragraph to display current charge */}
+                  {charges.operation && (
+                    <Typography
+                      variant="body1"
+                      style={{ marginTop: "6px", fontSize: "12px" }}
+                    >
+                      {currentCharge
+                        ? `Current Charge: ${currentCharge}.00`
+                        : //: "No charge available"}
+                          `Current Charge: `}
+                    </Typography>
+                  )}
 
                   <TextField
                     fullWidth
@@ -222,132 +281,3 @@ function Charges() {
 }
 
 export default Charges;
-
-
-
-{
-  /* <Snackbar
-        TransitionComponent={Slide}
-        open={passwordToast.error}
-        autoHideDuration={6000}
-        onClose={handleClose}
-      >
-        <Alert onClose={handleClose} severity="warning" sx={{ width: "100%" }}>
-          {updatePasswordMutation.error?.response?.data?.message}
-        </Alert>
-      </Snackbar>
-      <Snackbar
-        TransitionComponent={Slide}
-        open={passwordToast.success}
-        autoHideDuration={6000}
-        onClose={handleClose}
-      >
-        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
-          {updatePasswordMutation?.data?.data?.message}
-        </Alert>
-      </Snackbar> */
-}
-{
-  /* <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-        }}
-      >
-        <Container maxWidth="lg">
-          <Box sx={{ pt: 3 }}>
-            <form>
-              <Card>
-                <CardHeader subheader="" title="Update Charges" />
-                <Divider />
-                <CardContent> */
-}
-{
-  /* <TextField
-                    fullWidth
-                    label="Currency ID"
-                    margin="normal"
-                    name="currencyId"
-                    onChange={handleChargeChange}
-                    type="text"
-                    value={charges.currencyId}
-                    variant="outlined"
-                  /> */
-}
-{
-  /* <FormControl fullWidth>
-                    <InputLabel
-                      id="demo-dialog-select-label"
-                      style={{ marginTop: "18px" }}
-                    >
-                      Select a Currency
-                    </InputLabel>
-                    <Select
-                      labelId="demo-dialog-select-label"
-                      id="demo-dialog-select"
-                      value={charges.currencyId}
-                      onChange={handleChargeChange}
-                      input={<OutlinedInput label="Charge" />}
-                      fullWidth
-                      style={{ marginTop: "20px" }}
-                    >
-                      <MenuItem value={175}>NAIRA</MenuItem>
-                      <MenuItem value={250}>USD</MenuItem>
-                    </Select>
-                  </FormControl>
-                  <TextField
-                    fullWidth
-                    label="Operation"
-                    margin="normal"
-                    name="operation"
-                    onChange={handleChargeChange}
-                    type="text"
-                    value={charges.operation}
-                    variant="outlined"
-                  />
-                  <TextField
-                    fullWidth
-                    label="New Charge Value"
-                    margin="normal"
-                    name="newChargeValue"
-                    onChange={handleChargeChange}
-                    type="number"
-                    value={charges.newChargeValue}
-                    variant="outlined"
-                  />
-                </CardContent>
-                <Divider />
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "flex-end",
-                    p: 2,
-                  }}
-                >
-                  <LoadingButton
-                    variant="contained"
-                    color="primary"
-                    loading={updateChargesMutation.isLoading}
-                    disabled={
-                      charges.currencyId === "" ||
-                      charges.operation === "" ||
-                      charges.newChargeValue === 0
-                    }
-                    onClick={() => {
-                      updateChargesMutation.mutate(charges);
-                    }}
-                  >
-                    Update Charges
-                  </LoadingButton>
-                </Box>
-              </Card>
-            </form>
-          </Box>
-        </Container>
-      </Box>
-    </>
-  );
-}
-
-export default Charges; */
-}
