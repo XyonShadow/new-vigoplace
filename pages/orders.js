debb19636b23eb1b1c82075ea0f7ce39d82528a0
Dave <uchennadavid2404@gmail.com>
2023-12-28 14:03:36 +0100

    Testing

--- pages/orders.js
+++ pages/orders.js
@@ -1,255 +1,255 @@
-import React, { useMemo, useState, useEffect } from "react";
-import MaterialReactTable from "material-react-table";
-import { useRouter } from "next/router";
-import axios from "axios";
-import { format } from "date-fns";
-//Material UI Imports
-import {
-  IconButton,
-  Tooltip,
-  InputLabel,
-  Select,
-  MenuItem,
-  FormControl,
-} from "@mui/material";
-import RefreshIcon from "@mui/icons-material/Refresh";
+// import React, { useMemo, useState, useEffect } from "react";
+// import MaterialReactTable from "material-react-table";
+// import { useRouter } from "next/router";
+// import axios from "axios";
+// import { format } from "date-fns";
+// //Material UI Imports
+// import {
+//   IconButton,
+//   Tooltip,
+//   InputLabel,
+//   Select,
+//   MenuItem,
+//   FormControl,
+// } from "@mui/material";
+// import RefreshIcon from "@mui/icons-material/Refresh";
 
-//useQuery Imports
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQueryClient,
-  useQuery,
-  useMutation,
-} from "@tanstack/react-query";
-import { getSession, useSession } from "next-auth/react";
+// //useQuery Imports
+// import {
+//   QueryClient,
+//   QueryClientProvider,
+//   useQueryClient,
+//   useQuery,
+//   useMutation,
+// } from "@tanstack/react-query";
+// import { getSession, useSession } from "next-auth/react";
 
-const API_BASE_URL = "https://vigoplace.com/server";
-//const API_BASE_URL = "http://localhost:4000";
-export default function Orders() {
-  const router = useRouter();
-  const { userid } = router.query;
-  const queryClient = useQueryClient();
-  const getUser = useSession();
-  const user = getUser?.data?.user;
-  const [orders, setOrders] = useState([]);
-  const [orderCount, setOrderCount] = useState(0);
-  const [globalFilter, setGlobalFilter] = useState("");
-  const [status, setStatus] = React.useState("all");
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [isFetching, setIsFetching] = useState(false);
-  const [pagination, setPagination] = useState({
-    pageIndex: 1,
-    pageSize: 10,
-  });
+// const API_BASE_URL = "https://vigoplace.com/server";
+// //const API_BASE_URL = "http://localhost:4000";
+// export default function Orders() {
+//   const router = useRouter();
+//   const { userid } = router.query;
+//   const queryClient = useQueryClient();
+//   const getUser = useSession();
+//   const user = getUser?.data?.user;
+//   const [orders, setOrders] = useState([]);
+//   const [orderCount, setOrderCount] = useState(0);
+//   const [globalFilter, setGlobalFilter] = useState("");
+//   const [status, setStatus] = React.useState("all");
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [isError, setIsError] = useState(false);
+//   const [isFetching, setIsFetching] = useState(false);
+//   const [pagination, setPagination] = useState({
+//     pageIndex: 1,
+//     pageSize: 10,
+//   });
 
-  const handleStatus = (event) => {
-    setStatus(event.target.value);
-  };
+//   const handleStatus = (event) => {
+//     setStatus(event.target.value);
+//   };
 
-  const handleSearch = (event) => {
-    setGlobalFilter(event || "");
-  };
+//   const handleSearch = (event) => {
+//     setGlobalFilter(event || "");
+//   };
 
-  const fetchUserOrders = async () => {
-    setIsFetching(true);
-    setIsLoading(true);
-    try {
-      const { data } = await axios.get(
-        `${API_BASE_URL}/api/admin/console/users/orders?status=${status}&perPage=${pagination.pageSize}&page=${pagination.pageIndex}&search=${globalFilter}`,
-        {
-          headers: {
-            Authorization: user?.token,
-          },
-        }
-      );
+//   const fetchUserOrders = async () => {
+//     setIsFetching(true);
+//     setIsLoading(true);
+//     try {
+//       const { data } = await axios.get(
+//         `${API_BASE_URL}/api/admin/console/users/orders?status=${status}&perPage=${pagination.pageSize}&page=${pagination.pageIndex}&search=${globalFilter}`,
+//         {
+//           headers: {
+//             Authorization: user?.token,
+//           },
+//         }
+//       );
 
-      //console.log(data);
-      setOrders(data?.data?.totalUserOrders);
-      setOrderCount(data?.data?.count);
-    } catch (err) {
-      setIsError(true);
-      console.log(err, "err fetching orders");
-    } finally {
-      setIsLoading(false);
-      setIsFetching(false);
-    }
-  };
+//       //console.log(data);
+//       setOrders(data?.data?.totalUserOrders);
+//       setOrderCount(data?.data?.count);
+//     } catch (err) {
+//       setIsError(true);
+//       console.log(err, "err fetching orders");
+//     } finally {
+//       setIsLoading(false);
+//       setIsFetching(false);
+//     }
+//   };
 
-  useEffect(() => {
-    fetchUserOrders();
-  }, [userid, status, pagination, globalFilter]);
+//   useEffect(() => {
+//     fetchUserOrders();
+//   }, [userid, status, pagination, globalFilter]);
 
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: "OPSOId",
-        enableClickToCopy: false,
-        header: "Order Id",
-      },
-      {
-        accessorKey: "SellerFullName",
-        enableClickToCopy: false,
-        muiTableBodyCellProps: ({ cell }) => ({
-          style: {
-            cursor: "pointer",
-          },
-          onClick: () => {
-            //console.log(cell.getValue());
-            const userId = cell.row.original.OPSOUserId;
-            const url = `/user/${userId}`;
-            window.open(url, "_blank");
-          },
-          onMouseEnter: (e) => {
-            e.target.style.textDecoration = "underline";
-          },
-          onMouseLeave: (e) => {
-            e.target.style.textDecoration = "none";
-          },
-        }),
-        header: "Buyer",
-        id: "fullName",
-      },
-      {
-        accessorKey: "BuyerFullName",
-        enableClickToCopy: false,
-        muiTableBodyCellProps: ({ cell }) => ({
-          style: {
-            cursor: "pointer",
-          },
-          onClick: () => {
-            //console.log(cell.getValue());
-            const userId = cell.row.original.BuyerId;
-            const url = `/user/${userId}`;
-            window.open(url, "_blank");
-          },
-          onMouseEnter: (e) => {
-            e.target.style.textDecoration = "underline";
-          },
-          onMouseLeave: (e) => {
-            e.target.style.textDecoration = "none";
-          },
-        }),
-        header: "Seller",
-        id: "2fullName",
-      },
-      {
-        accessorKey: "OPSOProductId",
-        enableClickToCopy: false,
-        header: "Product Id",
-      },
-      {
-        accessorKey: "OPSOTransactionId",
-        enableClickToCopy: false,
-        header: "Transaction Id",
-      },
-      {
-        accessorKey: "OPSOStatus",
-        enableClickToCopy: false,
-        header: "Status",
-      },
-      {
-        accessorKey: "OPSOCurrencyId",
-        enableClickToCopy: false,
-        header: "Currency",
-      },
-      {
-        accessorKey: "OPSOQuantity",
-        enableClickToCopy: false,
-        header: "Order Quantity",
-      },
-      {
-        accessorKey: "OPSOAmount",
-        enableClickToCopy: false,
-        header: "Order Currency Amount",
-      },
-      {
-        accessorKey: "OPSOAddress",
-        enableClickToCopy: false,
-        header: "Address",
-      },
-      {
-        accessorFn: (row) => {
-          if (row?.OPSOCreatedAt) {
-            return format(new Date(row.OPSOCreatedAt), "MM/dd/yyyy hh:mm a");
-          } else {
-            return "";
-          }
-        },
-        enableClickToCopy: false,
-        header: "Date",
-      },
-    ],
-    []
-  );
+//   const columns = useMemo(
+//     () => [
+//       {
+//         accessorKey: "OPSOId",
+//         enableClickToCopy: false,
+//         header: "Order Id",
+//       },
+//       {
+//         accessorKey: "SellerFullName",
+//         enableClickToCopy: false,
+//         muiTableBodyCellProps: ({ cell }) => ({
+//           style: {
+//             cursor: "pointer",
+//           },
+//           onClick: () => {
+//             //console.log(cell.getValue());
+//             const userId = cell.row.original.OPSOUserId;
+//             const url = `/user/${userId}`;
+//             window.open(url, "_blank");
+//           },
+//           onMouseEnter: (e) => {
+//             e.target.style.textDecoration = "underline";
+//           },
+//           onMouseLeave: (e) => {
+//             e.target.style.textDecoration = "none";
+//           },
+//         }),
+//         header: "Buyer",
+//         id: "fullName",
+//       },
+//       {
+//         accessorKey: "BuyerFullName",
+//         enableClickToCopy: false,
+//         muiTableBodyCellProps: ({ cell }) => ({
+//           style: {
+//             cursor: "pointer",
+//           },
+//           onClick: () => {
+//             //console.log(cell.getValue());
+//             const userId = cell.row.original.BuyerId;
+//             const url = `/user/${userId}`;
+//             window.open(url, "_blank");
+//           },
+//           onMouseEnter: (e) => {
+//             e.target.style.textDecoration = "underline";
+//           },
+//           onMouseLeave: (e) => {
+//             e.target.style.textDecoration = "none";
+//           },
+//         }),
+//         header: "Seller",
+//         id: "2fullName",
+//       },
+//       {
+//         accessorKey: "OPSOProductId",
+//         enableClickToCopy: false,
+//         header: "Product Id",
+//       },
+//       {
+//         accessorKey: "OPSOTransactionId",
+//         enableClickToCopy: false,
+//         header: "Transaction Id",
+//       },
+//       {
+//         accessorKey: "OPSOStatus",
+//         enableClickToCopy: false,
+//         header: "Status",
+//       },
+//       {
+//         accessorKey: "OPSOCurrencyId",
+//         enableClickToCopy: false,
+//         header: "Currency",
+//       },
+//       {
+//         accessorKey: "OPSOQuantity",
+//         enableClickToCopy: false,
+//         header: "Order Quantity",
+//       },
+//       {
+//         accessorKey: "OPSOAmount",
+//         enableClickToCopy: false,
+//         header: "Order Currency Amount",
+//       },
+//       {
+//         accessorKey: "OPSOAddress",
+//         enableClickToCopy: false,
+//         header: "Address",
+//       },
+//       {
+//         accessorFn: (row) => {
+//           if (row?.OPSOCreatedAt) {
+//             return format(new Date(row.OPSOCreatedAt), "MM/dd/yyyy hh:mm a");
+//           } else {
+//             return "";
+//           }
+//         },
+//         enableClickToCopy: false,
+//         header: "Date",
+//       },
+//     ],
+//     []
+//   );
 
-  return (
-    <>
-      <MaterialReactTable
-        enableColumnFilterModes
-        enableColumnOrdering
-        enablePinning
-        columns={columns}
-        data={orders}
-        enableStickyHeader
-        enablePagination
-        manualPagination
-        manualFiltering
-        onPaginationChange={setPagination}
-        rowCount={orderCount}
-        onGlobalFilterChange={handleSearch}
-        initialState={{ showColumnFilters: false }}
-        positionToolbarAlertBanner="bottom"
-        muiToolbarAlertBannerProps={
-          isError
-            ? {
-                color: "error",
-                children:
-                  "Error loading data, Please use the refresh button on the table to retry",
-              }
-            : undefined
-        }
-        renderTopToolbarCustomActions={({ table }) => {
-          return (
-            <div style={{ display: "flex", gap: "0.5rem" }}>
-              <Tooltip arrow title="Refresh Data">
-                <IconButton onClick={() => fetchUserOrders()}>
-                  <RefreshIcon />
-                </IconButton>
-              </Tooltip>
+//   return (
+//     <>
+//       <MaterialReactTable
+//         enableColumnFilterModes
+//         enableColumnOrdering
+//         enablePinning
+//         columns={columns}
+//         data={orders}
+//         enableStickyHeader
+//         enablePagination
+//         manualPagination
+//         manualFiltering
+//         onPaginationChange={setPagination}
+//         rowCount={orderCount}
+//         onGlobalFilterChange={handleSearch}
+//         initialState={{ showColumnFilters: false }}
+//         positionToolbarAlertBanner="bottom"
+//         muiToolbarAlertBannerProps={
+//           isError
+//             ? {
+//                 color: "error",
+//                 children:
+//                   "Error loading data, Please use the refresh button on the table to retry",
+//               }
+//             : undefined
+//         }
+//         renderTopToolbarCustomActions={({ table }) => {
+//           return (
+//             <div style={{ display: "flex", gap: "0.5rem" }}>
+//               <Tooltip arrow title="Refresh Data">
+//                 <IconButton onClick={() => fetchUserOrders()}>
+//                   <RefreshIcon />
+//                 </IconButton>
+//               </Tooltip>
 
-              <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel id="demo-simple-select-standard-label">
-                  Status
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-standard-label"
-                  id="demo-simple-select-standard"
-                  value={status}
-                  defaultValue="None"
-                  onChange={handleStatus}
-                  label="Status"
-                >
-                  <MenuItem value={"all"}>All</MenuItem>
-                  <MenuItem value={"pending"}>Pending</MenuItem>
-                  <MenuItem value={"shipped"}>Shipped</MenuItem>
-                  <MenuItem value={"completed"}>Completed</MenuItem>
-                  <MenuItem value={"cancelled"}>Cancelled</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
-          );
-        }}
-        state={{
-          isLoading,
-          showAlertBanner: isError,
-          showProgressBars: isFetching,
-          pagination,
-          globalFilter,
-        }}
-        muiTableContainerProps={{ sx: { height: "75vh" } }}
-      />
-    </>
-  );
-}
+//               <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+//                 <InputLabel id="demo-simple-select-standard-label">
+//                   Status
+//                 </InputLabel>
+//                 <Select
+//                   labelId="demo-simple-select-standard-label"
+//                   id="demo-simple-select-standard"
+//                   value={status}
+//                   defaultValue="None"
+//                   onChange={handleStatus}
+//                   label="Status"
+//                 >
+//                   <MenuItem value={"all"}>All</MenuItem>
+//                   <MenuItem value={"pending"}>Pending</MenuItem>
+//                   <MenuItem value={"shipped"}>Shipped</MenuItem>
+//                   <MenuItem value={"completed"}>Completed</MenuItem>
+//                   <MenuItem value={"cancelled"}>Cancelled</MenuItem>
+//                 </Select>
+//               </FormControl>
+//             </div>
+//           );
+//         }}
+//         state={{
+//           isLoading,
+//           showAlertBanner: isError,
+//           showProgressBars: isFetching,
+//           pagination,
+//           globalFilter,
+//         }}
+//         muiTableContainerProps={{ sx: { height: "75vh" } }}
+//       />
+//     </>
+//   );
+// }
--- pages/post-categorization.js
+++ pages/post-categorization.js
@@ -1,99 +1,99 @@
-import { Postcategorization1 } from "../src/components/dashboard/Postcategorization1";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useQuery } from "@tanstack/react-query";
-import { useState, useEffect, useRef } from "react";
+// import { Postcategorization1 } from "../src/components/dashboard/Postcategorization1";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { useQuery } from "@tanstack/react-query";
+// import { useState, useEffect, useRef } from "react";
 
-const API_BASE_URL = "https://vigoplace.com/server";
-//const API_BASE_URL = "http://localhost:4000";
-const PostCategorization = () => {
-  //const [categorizedData, setCategorizedData] = useState([]);
-  //const [unCategorizedData, setUncategorizedData] = useState([]);
-  // const [currentPage, setCurrentPage] = useState(1);
-  // const [pageSize, setPageSize] = useState(4); // Fetch 1 item at a time
-  // //When fetching the next data, Change the currentPage to 2
+// const API_BASE_URL = "https://vigoplace.com/server";
+// //const API_BASE_URL = "http://localhost:4000";
+// const PostCategorization = () => {
+//   //const [categorizedData, setCategorizedData] = useState([]);
+//   //const [unCategorizedData, setUncategorizedData] = useState([]);
+//   // const [currentPage, setCurrentPage] = useState(1);
+//   // const [pageSize, setPageSize] = useState(4); // Fetch 1 item at a time
+//   // //When fetching the next data, Change the currentPage to 2
 
-  // const API_BASE_URL = "https://vigoplace.com/server";
-  // 
+//   // const API_BASE_URL = "https://vigoplace.com/server";
+//   // 
 
-  // const fetchData = async () => {
-  //   const response = await fetch(
-  //     `${API_BASE_URL}/api/admin/categorized?page=${currentPage}&itemsPerPage=${pageSize}`
-  //   );
-  //   if (!response.ok) {
-  //     throw new Error("Failed to fetch data");
-  //   }
-  //   const data = await response.json();
-  //   //console.log(data.data[0]);
-  //   return data?.data?.[0];
-  // };
+//   // const fetchData = async () => {
+//   //   const response = await fetch(
+//   //     `${API_BASE_URL}/api/admin/categorized?page=${currentPage}&itemsPerPage=${pageSize}`
+//   //   );
+//   //   if (!response.ok) {
+//   //     throw new Error("Failed to fetch data");
+//   //   }
+//   //   const data = await response.json();
+//   //   //console.log(data.data[0]);
+//   //   return data?.data?.[0];
+//   // };
 
-  // const queryKey = ["categorizedPost", currentPage, pageSize];
+//   // const queryKey = ["categorizedPost", currentPage, pageSize];
 
-  // const {
-  //   data: categorizedItem,
-  //   isLoading,
-  //   isError,
-  //   isSuccess,
-  // } = useQuery(queryKey, () => fetchData(currentPage, pageSize), {
+//   // const {
+//   //   data: categorizedItem,
+//   //   isLoading,
+//   //   isError,
+//   //   isSuccess,
+//   // } = useQuery(queryKey, () => fetchData(currentPage, pageSize), {
 
-  //   onSuccess: (data) => {
-  //     const newData = data || [];
-  //     setCategorizedData((prevData) => [...prevData, ...newData]);
-  //   },
-  // });
+//   //   onSuccess: (data) => {
+//   //     const newData = data || [];
+//   //     setCategorizedData((prevData) => [...prevData, ...newData]);
+//   //   },
+//   // });
 
-  // const unCategorizedPostsQuery = useQuery({ queryKey: ["uncategorizedData"], queryFn: fetchData,
-  //   refetchInterval: 10000,
-  //   refetchOnMount: 'always'
-  // });
+//   // const unCategorizedPostsQuery = useQuery({ queryKey: ["uncategorizedData"], queryFn: fetchData,
+//   //   refetchInterval: 10000,
+//   //   refetchOnMount: 'always'
+//   // });
 
-  // useEffect(() =>  {
-  //   if (unCategorizedPostsQuery.data) {
-  //     setUncategorizedData(unCategorizedPostsQuery.data)
-  //     console.log("This one na for ujseefect")
-  //   }
-  // }, [unCategorizedPostsQuery.data])
+//   // useEffect(() =>  {
+//   //   if (unCategorizedPostsQuery.data) {
+//   //     setUncategorizedData(unCategorizedPostsQuery.data)
+//   //     console.log("This one na for ujseefect")
+//   //   }
+//   // }, [unCategorizedPostsQuery.data])
 
-  const currentPage = useRef(1);
-  const pageSize = useRef(50);
-  const [categorizedData, setCategorizedData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [isError, setIsError] = useState(null);
+//   const currentPage = useRef(1);
+//   const pageSize = useRef(50);
+//   const [categorizedData, setCategorizedData] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [isError, setIsError] = useState(null);
 
 
-  // fetch categorized data
-  const fetchCatgorizedData = async () => {
-    const response = await fetch(
-      `${API_BASE_URL}/api/admin/categorized?page=${currentPage.current}&itemsPerPage=${pageSize.current}`
-    );
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-    const data = await response.json();
-    //console.log(...data?.data);
-    //setCategorizedData((prev) => [...prev, ...data?.data?.[0]]);
-    setCategorizedData((prev) => [...prev, ...data?.data]);
-    setLoading(false);
-  };
+//   // fetch categorized data
+//   const fetchCatgorizedData = async () => {
+//     const response = await fetch(
+//       `${API_BASE_URL}/api/admin/categorized?page=${currentPage.current}&itemsPerPage=${pageSize.current}`
+//     );
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch data");
+//     }
+//     const data = await response.json();
+//     //console.log(...data?.data);
+//     //setCategorizedData((prev) => [...prev, ...data?.data?.[0]]);
+//     setCategorizedData((prev) => [...prev, ...data?.data]);
+//     setLoading(false);
+//   };
 
-  useEffect(() => {
-    fetchCatgorizedData();
-  }, []);
+//   useEffect(() => {
+//     fetchCatgorizedData();
+//   }, []);
 
-  return (
-    <div>
-      <ToastContainer position="top-center" />
-      <Postcategorization1
-        categorizedData={categorizedData}
-        isLoading={loading}
-        isError={isError}
-        currentPage={currentPage}
-        fetchCatgorizedData={fetchCatgorizedData}
-      />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <ToastContainer position="top-center" />
+//       <Postcategorization1
+//         categorizedData={categorizedData}
+//         isLoading={loading}
+//         isError={isError}
+//         currentPage={currentPage}
+//         fetchCatgorizedData={fetchCatgorizedData}
+//       />
+//     </div>
+//   );
+// };
 
-PostCategorization.auth = true;
-export default PostCategorization;
+// PostCategorization.auth = true;
+// export default PostCategorization;
