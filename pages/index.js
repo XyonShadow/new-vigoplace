d3878d5c93d36248d2a34050d3e790fc178f1619
Dave <uchennadavid2404@gmail.com>
2025-07-11 03:15:07 +0100

    check now

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Login() {
             {/* <LockOutlinedIcon /> */}
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Sign in now
           </Typography>
           <Box
             component="form"
