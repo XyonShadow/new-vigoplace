ef4a11105416bbcdeffd354379fbae8d84ea4f26
Dave <uchennadavid2404@gmail.com>
2025-07-11 03:36:13 +0100

    check

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Login() {
             {/* <LockOutlinedIcon /> */}
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in now
+            Sign in now now
           </Typography>
           <Box
             component="form"
@@ -109,7 +109,7 @@ export default function Login() {
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In now
+              Sign In now now
             </Button>
             {/* <Grid container>
               <Grid item xs>
