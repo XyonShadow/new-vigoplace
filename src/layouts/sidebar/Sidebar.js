1f45d4dca23a90138de6b2920cd6d1571f447365
Michael <prncemk11@gmail.com>
2025-09-25 21:58:39 +0100

    Fixed sidebar height issue

--- src/layouts/sidebar/Sidebar.js
+++ src/layouts/sidebar/Sidebar.js
@@ -526,6 +526,35 @@ function Sidebar({ isMobileSidebarOpen, onSidebarClose, isSidebarOpen }) {
         },
       ],
     },
+    {
+      id: 31,
+      title: "Traction",
+      icon: "credit-card",
+      href: "",
+      roles: ["admin", "root"],
+      SCPCreatedAt: "2024-04-29T00:00:00.000Z",
+      SCPUpdatedAt: null,
+      subLinks: [
+        {
+          id: 32,
+          title: "KPI",
+          icon: "activity",
+          href: "/kpi",
+          roles: ["admin", "superAdmin", "root"],
+          SCPCreatedAt: "2024-05-03T11:21:15.000Z",
+          SCPUpdatedAt: null,
+        },
+        {
+          id: 33,
+          title: "User Revenues",
+          icon: "activity",
+          href: "/user-revenue",
+          roles: ["admin", "root"],
+          SCPCreatedAt: "2024-11-27T06:46:44.000Z",
+          SCPUpdatedAt: null,
+        },
+      ]
+    },
     // {
     //   id: 23,
     //   title: "Virtual-Accounts",
@@ -593,8 +622,9 @@ function Sidebar({ isMobileSidebarOpen, onSidebarClose, isSidebarOpen }) {
   const roleIdToFind = openDropdown;
   const subLinksCount = countSubLinksById(fetchedRoles, roleIdToFind);
 
-  const dynamicHeight =
-    openDropdown !== null ? subLinksCount * 64 + screenHeight : screenHeight;
+  const dynamicHeight = openDropdown !== null 
+    ? `${subLinksCount * 64 + screenHeight}px` 
+    : "100vh";
 
   //const sidebarMenu = fetchedRoles
   const sidebarMenu = fetchedRoles
@@ -638,7 +668,9 @@ function Sidebar({ isMobileSidebarOpen, onSidebarClose, isSidebarOpen }) {
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-between",
+        minHeight: "100vh",
         height: dynamicHeight,
+        overflowY: "auto",
       }}
     >
       <Box
@@ -914,6 +946,7 @@ function Sidebar({ isMobileSidebarOpen, onSidebarClose, isSidebarOpen }) {
           display: "flex",
           alignItems: "center",
           alignSelf: "Center",
+          flexShrink: 0,
         }}
       >
         <Button
